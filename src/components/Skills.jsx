49882d97e305803564e7fc063251e8c5951c1e2b
AbimaelG <azg_96@hotmail.com>
2022-09-25 05:01:29 -0500

    Introduction text about me finished in "About me section"

---
+++ src/components/Skills.jsx
@@ -0,0 +1,8 @@
+
+function Skills () {
+  return (
+    <div>hola</div>
+  )
+}
+
+export default Skills
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,24 @@
+import style from '../styles/pages/AboutMe.module.scss'
+import Skills from '../components/Skills'
+
 function AboutMe () {
   return (
-    <h1>About Me</h1>
+    <h1 className={style['am-container']}>
+      <div className={style['am-container_introduction']}>
+        <h1 className={style['am-container_introduction_title']}>About <span>me</span></h1>
+        <p className={style['am-container_introduction_subtitle']}>- A little bit about my self</p>
+        <p className={style['am-container_introduction_text']}>
+        Hi there! <br /><br />
+        My name is Abimael Zavala Guerrero I am 26 year old, I am Mexican and I am from Puebla City, Puebla.<br /><br />
+        I studied graphic design from 2014 to 2019 but 1 year before finishing college I felt atracted to code so I decided to learn just by self-teaching.</p>
+      </div>
+      <h2>About my Skills</h2>
+      <div>
+        <Skills />
+        <Skills />
+        <Skills />
+      </div>
+    </h1>
   )
 }
 
