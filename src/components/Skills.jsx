15ac813a13d72d6bcbb1cf483d4b33798c67adeb
AbimaelG <azg_96@hotmail.com>
2022-09-26 04:38:21 -0500

    Sections for each skill finished

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,32 @@
+import style from '../styles/components/Skills.module.scss'
 
-function Skills () {
+function Skills ({ orangeText, firstSkill, secondSkill, thirdSkill, forthSkill, fifthSkill, firstSkillXp, secondSkillXp, thirdSkillXp, forthSkillXp, fifthSkillXp, currentlyLearning }) {
   return (
-    <div>hola</div>
+    <div className={style['am-container__skills']}>
+      <div className={style['am-container__skills__titles']}>
+        <h1 className={style['am-container__skills__titles__main-title']}><span>{orangeText}</span> Skills</h1>
+        <p className={style['am-container__skills__titles__xp-title']}>
+          Time of<br />
+          Experience
+        </p>
+      </div>
+      <div className={style['am-container__skills__content']}>
+        <ul className={style['am-container__skills__content__items']}>
+          <li>- {firstSkill}</li>
+          <li>- {secondSkill}</li>
+          <li>- {thirdSkill}</li>
+          <li>- {forthSkill}</li>
+          <li>- {fifthSkill}{currentlyLearning}</li>
+        </ul>
+        <ul className={style['am-container__skills__content__xp']}>
+          <li>{firstSkillXp}</li>
+          <li>{secondSkillXp}</li>
+          <li>{thirdSkillXp}</li>
+          <li>{forthSkillXp}</li>
+          <li>{fifthSkillXp}</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,19 +4,56 @@ import Skills from '../components/Skills'
 function AboutMe () {
   return (
     <h1 className={style['am-container']}>
-      <div className={style['am-container_introduction']}>
-        <h1 className={style['am-container_introduction_title']}>About <span>me</span></h1>
-        <p className={style['am-container_introduction_subtitle']}>- A little bit about my self</p>
-        <p className={style['am-container_introduction_text']}>
+      <div className={style['am-container__introduction']}>
+        <h1 className={style['am-container__introduction__title']}>About <span>me</span></h1>
+        <p className={style['am-container__introduction__subtitle']}>- A little bit about my self</p>
+        <p className={style['am-container__introduction__text']}>
         Hi there! <br /><br />
         My name is Abimael Zavala Guerrero I am 26 year old, I am Mexican and I am from Puebla City, Puebla.<br /><br />
         I studied graphic design from 2014 to 2019 but 1 year before finishing college I felt atracted to code so I decided to learn just by self-teaching.</p>
       </div>
-      <h2>About my Skills</h2>
+      <h2 className={style['am-container__about-my-skills-text']}>About my <span>Skills</span></h2>
       <div>
-        <Skills />
-        <Skills />
-        <Skills />
+        <Skills
+          orangeText='Key'
+          firstSkill='HTML'
+          secondSkill='CSS'
+          thirdSkill='JavaScript'
+          forthSkill='Sass'
+          fifthSkill='React.js'
+          firstSkillXp='3 years'
+          secondSkillXp='3 years'
+          thirdSkillXp={'1.3 years'}
+          forthSkillXp='1 year'
+          fifthSkillXp='4 months'
+          currentlyLearning={<span>(Currently Learning)</span>}
+        />
+          <Skills
+            orangeText='Extra'
+            firstSkill='CSS Modules'
+            secondSkill={'Git & Github'}
+            thirdSkill='Eslint'
+            forthSkill='NPM'
+            fifthSkill='BEM methodology'
+            firstSkillXp='4 months'
+            secondSkillXp='1 year'
+            thirdSkillXp='3 months'
+            forthSkillXp='1 year'
+            fifthSkillXp='3 months'
+          />
+        <Skills
+          orangeText={<span>Graphic<br />Design</span>}
+          firstSkill='Adobe Illustrator'
+          secondSkill='Adobe Photoshop'
+          thirdSkill='Adobe After Effect'
+          forthSkill='Adobe Premiere'
+          fifthSkill='Adobe XD'
+          firstSkillXp='4 years'
+          secondSkillXp='4 years'
+          thirdSkillXp='2 years'
+          forthSkillXp='1 year'
+          fifthSkillXp='1 year'
+        />
       </div>
     </h1>
   )
